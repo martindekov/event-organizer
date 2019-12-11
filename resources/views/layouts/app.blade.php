ac7136527ea13b73c1e4ce8b9701f11992530eae
Kiril <53880262+YakimovB@users.noreply.github.com>
2019-12-11 03:32:36 +0200

    nav bar active

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,18 +48,18 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
+                    <ul class="navbar-nav ml-auto nav-pills">
 
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/home') }}">{{ __('Events') }}</a>
+                            <a class="nav-link {{ Request::segment(1) === 'home' ? 'active text-white' : null }}" href="{{ url('home') }}">{{ __('Events') }}</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/about') }}">{{ __('About') }}</a>
+                            <a class="nav-link {{ Request::segment(1) === 'about' ? 'active text-white' : null }}" href="{{ url('about') }}">{{ __('About') }}</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/contact') }}">{{ __('Contacts') }}</a>
+                            <a class="nav-link {{ Request::segment(1) === 'contact' ? 'active text-white' : null }}" href="{{ url('contact') }}">{{ __('Contacts') }}</a>
                         </li>
 
                         <!-- Authentication Links -->
@@ -69,10 +69,10 @@
 
                         @elseif (Request::path() != 'login' && Route::has('register'))
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <a class="nav-link {{ Request::segment(1) === 'login' ? 'active text-white' : null }}" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
+                            <a class="nav-link {{ Request::segment(1) === 'register' ? 'active text-white' : null }}" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
                         </li>
                         @endif
 
